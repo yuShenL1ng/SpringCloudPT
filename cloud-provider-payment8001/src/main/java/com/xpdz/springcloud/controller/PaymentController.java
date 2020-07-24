ec0f7976b851afb6b06c8bf07527f98c64570046
yuShenL1ng <474296307@qq.com>
2020-07-24 17:29:03 +0800

    框架搭建

---
+++ cloud-provider-payment8001/src/main/java/com/xpdz/springcloud/controller/PaymentController.java
@@ -0,0 +1,39 @@
+package com.xpdz.springcloud.controller;
+
+import com.xpdz.springcloud.entities.CommonResult;
+import com.xpdz.springcloud.entities.Payment;
+import com.xpdz.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping("/create")
+    public CommonResult create(Payment payment){
+        int result = paymentService.create(payment);
+        log.info("****插入结果: "+result);
+        if(result>0){
+            return new CommonResult(200,"插入成功",result);
+        }else {
+            return new CommonResult(444,"插入失败");
+        }
+    }
+
+    @GetMapping("/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id){
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("****插入结果: "+payment);
+        if(payment!=null){
+            return new CommonResult(200,"查询成功",payment);
+        }else {
+            return new CommonResult(444,"无记录");
+        }
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/xpdz/springcloud/service/PaymentService.java
@@ -0,0 +1,9 @@
+package com.xpdz.springcloud.service;
+
+import com.xpdz.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+
+public interface PaymentService {
+    int create(Payment payment);
+    Payment getPaymentById(@Param("id") Long id);
+}
